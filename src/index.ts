712f4818fb06901ca18ae81b35553c2619c74dc7
Bruno Santos <brunosps@hotmail.com>
2024-09-06 15:49:32 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-
-export { Failure } from './Failure'
-export { Success } from './Success'
-export { Result } from './Result'
-export { ResultProps, TransitionType } from './Types'
-export { UseCase } from './UseCase'
+export { Failure } from './Failure';
+export { Result } from './Result';
+export { Success } from './Success';
+export { ResultProps, TransitionType } from './Types';
+export { UseCase } from './UseCase';
