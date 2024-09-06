7b9fd4f4d1b6bd4ce81040d90b512b29b2fa9902
Bruno Santos <brunosps@hotmail.com>
2024-09-06 15:47:58 -0300

    Update Result.ts

--- src/Result.ts
+++ src/Result.ts
@@ -1,116 +1,111 @@
-import { Failure } from "./Failure";
-import { ResultProps, TransitionType } from "./Types";
+import { Failure } from './Failure';
+import { ResultProps } from './Types';
 
 export class Result<T> {
-    resultType: string;
-    data: T
-    error: Error;
-    useCaseClass: string;
-    context: any;
-    transitions: TransitionType[];
+  resultType: string;
+  data: T;
+  error: Error;
+  context: any;
+  useCaseClass: string;
 
-    private _isSuccess: boolean;
+  private _isSuccess: boolean;
 
-    constructor({
-        resultType,
-        isSuccess,
-        error,
-        data,
-        context,
-        useCaseClass,
-        transitions }: ResultProps<T>) {
+  constructor({
+    resultType,
+    isSuccess,
+    error,
+    data,
+    context,
+    useCaseClass,
+  }: ResultProps<T>) {
+    this.resultType = resultType;
+    this._isSuccess = isSuccess;
+    this.data = {} as T;
+    this.error = {} as Error;
+    this.useCaseClass = '';
 
-        this.resultType = resultType;
-        this._isSuccess = isSuccess;
-        this.data = {} as T;
-        this.useCaseClass = "";
-        this.error = {} as Error;
-
-        if (data) {
-            this.data = data;
-        }
-
-        if (error) {
-            this.error = error;
-        }
-
-        if (useCaseClass) {
-            this.useCaseClass = useCaseClass;
-        }
-
-        if (context) {
-            this.context = context;
-        }
-
-        this.transitions = [];
-
-        if (transitions) {
-            this.transitions = transitions;
-        }
-
-        Object.freeze(this);
+    if (data) {
+      this.data = data;
     }
 
-    getValue(): T {
-        return this.data;
+    if (error) {
+      this.error = error;
     }
 
-    isSuccess() {
-        return this._isSuccess
+    if (useCaseClass) {
+      this.useCaseClass = useCaseClass;
     }
 
-    isFailure() {
-        return !this._isSuccess
+    if (context) {
+      this.context = context;
     }
 
-    getType() {
-        return this.resultType;
+    Object.freeze(this);
+  }
+
+  getValue(): T {
+    return this.data;
+  }
+
+  isSuccess() {
+    return this._isSuccess;
+  }
+
+  isFailure() {
+    return !this._isSuccess;
+  }
+
+  getType() {
+    return this.resultType;
+  }
+
+  getError() {
+    return this.error;
+  }
+
+  async execUseCase<U>(
+    f: (data: T, res: Result<T>) => Promise<Result<U>>,
+  ): Promise<Result<U>> {
+    if (this.isFailure()) {
+      return Failure(
+        this.error,
+        this.resultType,
+        this.context,
+        this.useCaseClass,
+      );
     }
 
-    getError() {
-        return this.error;
+    const result = await f(this.getValue(), this);
+    return this.mergeContext<U>(result, this);
+  }
+
+  private mergeContext<U>(result: Result<U>, mergeable: Result<T>) {
+    return new Result<U>({
+      resultType: result.resultType,
+      isSuccess: result._isSuccess,
+      error: result.error,
+      data: result.data,
+      useCaseClass: result.useCaseClass,
+      context: { ...mergeable.context, ...result.context },
+    });
+  }
+
+  onSuccess(f: (data: T, res: Result<T>) => any): Result<T> {
+    if (this._isSuccess) {
+      f(this.getValue(), this);
     }
 
-    getContext() {
-        return this.context;
+    return this;
+  }
+
+  onFailure(
+    f: (error: Error, res: Result<T>) => any,
+    failureType = 'FAILURE',
+  ): Result<T> {
+    if (this.isFailure() && this.resultType === failureType) {
+      f(this.getError(), this);
     }
 
-    andThen<U>(f: (data: T, res: Result<T>) => Result<U>): Result<U> {
-        if (this.isFailure()) {
-            return Failure(this.error, this.resultType, this.context, this.useCaseClass, this.transitions);
-        }
-
-        const result = f(this.getValue(), this);
-
-        return this.mergeContext<U>(result, this);
-    }
-
-    mergeContext<U>(result: Result<U>, mergeable: Result<T>) {
-        return new Result<U>({
-            resultType: result.resultType,
-            isSuccess: result._isSuccess,
-            error: result.error,
-            data: result.data,
-            useCaseClass: result.useCaseClass,
-            context: { ...mergeable.context, ...result.context },
-            transitions: [...mergeable.transitions, ...result.transitions],
-        })
-    }
-
-    onSuccess(f: (data: T, res: Result<T>) => any): Result<T> {
-        if (this._isSuccess) {
-            f(this.getValue(), this);
-        }
-
-        return this;
-    }
-
-    onFailure(f: (error: Error, res: Result<T>) => any, failureType = "FAILURE"): Result<T> {
-        if (this.isFailure() && this.resultType === failureType) {
-            f(this.getError(), this);
-        }
-
-        return this;
-    }
-
+    return this;
+  }
 }
