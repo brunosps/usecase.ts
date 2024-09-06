0fdb38b11cea7e037f6af6538a84b38556d1ebb0
Bruno Santos <brunosps@hotmail.com>
2024-09-06 15:47:35 -0300

    Update Success.ts

--- src/Success.ts
+++ src/Success.ts
@@ -1,15 +1,15 @@
-import { Result } from "./Result";
-import { TransitionType } from "./Types";
+import { Result } from './Result';
 
-export const Success = <U,>(value?: U, context?: any, useCaseClass?: string, transitions?: TransitionType[]): Result<U> => {
-    return new Result<U>(
-        {
-            resultType: "SUCCESS",
-            isSuccess: true,
-            data: value,
-            context: context,
-            useCaseClass: useCaseClass,
-            transitions,
-        },
-    );
-}
+export const Success = <U>(
+  value?: U,
+  context?: any,
+  useCaseClass?: string,
+): Result<U> => {
+  return new Result<U>({
+    resultType: 'SUCCESS',
+    isSuccess: true,
+    data: value,
+    context,
+    useCaseClass,
+  });
+};
