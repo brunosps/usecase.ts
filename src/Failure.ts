7c313281afebcad320eb501408715d17882d8394
Bruno Santos <brunosps@hotmail.com>
2024-09-06 15:48:15 -0300

    Update Failure.ts

--- src/Failure.ts
+++ src/Failure.ts
@@ -1,14 +1,16 @@
+import { Result } from './Result';
 
-import { Result } from "./Result"
-import { TransitionType } from "./Types"
-
-export const Failure = <U,>(error: Error, failureType = "FAILURE", context?: any, useCaseClass?: string, transitions?: TransitionType[]): Result<U> => {
-    return new Result<U>({
-        resultType: failureType,
-        isSuccess: false,
-        error,
-        context,
-        useCaseClass,
-        transitions,
-    })
-}
+export const Failure = <U>(
+  error: Error,
+  failureType = 'FAILURE',
+  context?: any,
+  useCaseClass?: string,
+): Result<U> => {
+  return new Result<U>({
+    resultType: failureType,
+    isSuccess: false,
+    error,
+    context,
+    useCaseClass,
+  });
+};
